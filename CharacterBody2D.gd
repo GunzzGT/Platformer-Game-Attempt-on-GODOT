d5a241c06278f5388d3c44d65dcd9dce27a37d0d
GunzzGT <gunawantandy@gmail.com>
2023-05-30 16:05:20 +0700

    Slight update
    Grouped inputs
    still trying to figure out how to stop animation, haven't tried using animation tree yet this is still very raw and unrefined

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,43 +1,43 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var run_speed = 350
+var jump_speed = -1000
+var gravity = 2500
 @onready var _animated_sprite = $AnimatedSprite2D
 
-func _physics_process(delta):
+func get_input():
+	velocity.x = 0
+	var right = Input.is_action_pressed('ui_right')
+	var left = Input.is_action_pressed('ui_left')
+	var jump = Input.is_action_pressed('ui_select')
 	
-	var direction = Input.get_axis("ui_left", "ui_right")
-	# Get the input direction and handle the movement/deceleration.
-	if direction:
-		$AnimatedSprite2D.flip_h = direction < 0
-		# control sprite direction
+	if is_on_floor() and jump:
+		velocity.y = jump_speed
+		$AnimatedSprite2D.play("sprite2-jump")
+	elif right:
+		$AnimatedSprite2D.flip_h = false
+		velocity.x += run_speed
 		if is_on_floor():
 			$AnimatedSprite2D.play("sprite2-run")
-			# control sprite change to run on floor
 		else:
 			$AnimatedSprite2D.play("sprite2-jump")
-			# control sprite change to jump on jump
-		velocity.x = direction * SPEED
+	elif left:
+		$AnimatedSprite2D.flip_h = true
+		velocity.x -= run_speed
+		if is_on_floor():
+			$AnimatedSprite2D.play("sprite2-run")
+		else:
+			$AnimatedSprite2D.play("sprite2-jump")
 	else:
 		if is_on_floor():
-			_animated_sprite.play("sprite2-idle")
-			#back to idle state
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-	move_and_slide()
-	
-	# Add the gravity.
-	if not is_on_floor():
-		# haven't implement fall animation
-		velocity.y += gravity * delta
+			$AnimatedSprite2D.play("sprite2-idle")
+		else:
+			$AnimatedSprite2D.play("sprite2-jump")
 
-	# Handle Jump.
-	if Input.is_action_pressed("ui_accept"):
-		# changed from action_just_pressed
-		_animated_sprite.play("sprite2-jump")
-		#control sprite jump
-		if is_on_floor():
-			velocity.y = JUMP_VELOCITY
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	get_input()
+	move_and_slide()
+
---
+++ RigidBody2D.gd
@@ -0,0 +1,8 @@
+extends RigidBody2D
+
+var velocity = Vector2(250, 250)
+
+func _physics_process(delta):
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		velocity = velocity.bounce(collision_info.get_normal())
